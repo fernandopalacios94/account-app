9d3b70e315c4237c2d6fe4bbe9e429ed13847a92
Fernando Palacios <fpalacios0894@hotmail.com>
2018-04-27 07:43:40 -0500

    Creation of the database

---
+++ config/database/create_database.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host:       'localhost',
+	user:       'root',
+	password:   'root',
+	socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+});
+
+con.connect(function(err) {
+	if (err) {
+		console.log('Error at line 11');
+		throw err;
+	}
+	console.log('Connected!');
+	con.query('CREATE DATABASE accounts', function (err, result) {
+		if (err) throw err;
+		console.log('Database created');
+	});
+});
